8beae61cc92baf33e3920acbd1017965005e04fa
amyma17 <amyma171311@gmail.com>
2018-06-20 13:49:51 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,13 @@
-def using_until
-        levitation_force = 6
-        #your code here
-    
+def using_while
+	levitation_force = 6
+	
+	loop do
+	  if levitation_force == 10 
+	    break
+  end
+puts "wingardium Leviosa"
+levitation_force = levitation_force+ 1
+end 
 end
+using_while
 
